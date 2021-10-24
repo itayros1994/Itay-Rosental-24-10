8e9c91d52da4db89cac227d687b983254ff6d6ad
itayros1994 <briskeritay3@Gmail.com>
2021-10-24 21:42:55 +0300

    apis req with axios

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,6 +1,9 @@
-import autoComplete from '../data/autoCompleteRespons.json'
-import currentWeather from '../data/currentWeather.json'
-import dailyForecast from '../data/5DaysForecasts.json'
+// import autoComplete from '../data/autoCompleteRespons.json'
+// import currentWeather from '../data/currentWeather.json'
+// import dailyForecast from '../data/5DaysForecasts.json'
+import axios from 'axios'
+
+const API_KEY = '0NGDR7qbB3jAERFSSyArpMpe4Zq3TCQV'
 
 export const WeatherService = {
     getLocations,
@@ -9,14 +12,22 @@ export const WeatherService = {
 }
 
 function getLocations(location) {
-    return Promise.resolve(autoComplete)
+    // return Promise.resolve(autoComplete)
+    return axios
+        .get(`http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${API_KEY}&q=${location}&language=en`)
+        .then(res => res.data)
 }
 
 function getCurrentWeather(locationKey) {
-    return Promise.resolve(currentWeather)
+    // return Promise.resolve(currentWeather)
+    return axios
+        .get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${API_KEY}&language=en&details=false`)
+        .then(res => res.data)
 }
 
 function getDailyForecast(locationKey) {
-    return Promise.resolve(dailyForecast)
+    // return Promise.resolve(dailyForecast)
+    return axios
+        .get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${API_KEY}&language=en&details=true&metric=true"`)
+        .then(res => res.data.DailyForecasts)
 }
-
